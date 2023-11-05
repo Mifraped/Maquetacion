1c6c33cdf3199e4110d69e4cdd113abf8e3d0a82
Miguel Franco <francamente@hotmail.es>
2023-11-05 13:01:47 +0100

    pelis y prof desde array

--- proyectoFinalFrontEnd/index.js
+++ proyectoFinalFrontEnd/index.js
@@ -1,34 +1,65 @@
 import { Movie } from "./Movie.js"
+import { Professional } from "./professional.js"
+
 let arrPelis = [
-    new Movie("La vida de Brian", 1979, "Reino Unido", "Terry Jones", "Graham Chapman", "Comedia", '"./imagenes/la_vida_de_brian.jfif"')
+    new Movie("La vida de Brian", 1979, "Reino Unido", "Terry Jones", "Graham Chapman", "Comedia", "./imagenes/la_vida_de_brian.jfif"),
+    new Movie("Regreso al futuro", 1985, "Estados Unidos", "Robert Zemeckis", "Robert Zemeckis", "Ciencia Ficcion", "./imagenes/regreso.jfif"),
+    new Movie("As bestas", 2022, "España", "Rodrigo Sorogoyen", "Isabel Peña", "Drama", "./imagenes/bestas.jfif")
+]
+
+let arrProf = [
+    new Professional("John Cleese", 84, "Britanica", 0, "Actor", "Si", "./imagenes/John_Cleese_(cropped).jpg"),
+    new Professional("Christopher LLoid", 85, "Estadounidense", 0, "Actor", "Si", "./imagenes/Christopher_Lloyd_May_2015.jpg"),
+    new Professional("Luis Zahera", 57, "", 0, "Actor", "No", "./imagenes/luis_zahera.jpg")
 ]
 
 jQuery(() => {
 
-    $(function () {
+    $(() => {
         for (let peli of arrPelis) {
-            let peliculaHtml = `<div class="card mb-3 bg-secondary d-flex justify-content-center align-items-center"
-        style="max-width: 400px;">
-        <div class="row g-0">
-            <div class="col-md-4 ps-2 d-flex justify-content-center align-items-center foto">
-                <img src="./imagenes/la_vida_de_brian.jfif" class="img-fluid rounded-start" alt="img">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title text-black">${peli.title}</h5>
-                    <h6 class="fs-6">Año: ${peli.releaseYear}</h6>
-                    <h6 class="fs-6">Actores: John Cleese, Graham Chapman</h6>
-                    <h6 class="fs-6">Nacionalidad: ${peli.nacionality}</h6>
-                    <h6 class="fs-6">Director: ${peli.director}</h6>
-                    <h6 class="fs-6">Guionista: ${peli.writer}</h6>
-                    <h6 class="fs-6">Genero: ${peli.genre}</h6>
-                </div>
-            </div>
-        </div>
-    </div>`
-            $(".contCuerpo").append(peliculaHtml)
+            $(".cuerpoPeli").append(`<div class="card mb-3 bg-secondary d-flex justify-content-center align-items-center mb-5"
+                                        style="max-width: 400px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 ps-2 d-flex justify-content-center align-items-center foto">
+                                                <img src="${peli.photo}" class="img-fluid rounded-start" alt="img">
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title text-black">${peli.title}</h5>
+                                                    <h6 class="fs-6">Año: ${peli.releaseYear}</h6>
+                                                    <h6 class="fs-6">Actores: John Cleese, Graham Chapman</h6>
+                                                    <h6 class="fs-6">Nacionalidad: ${peli.nacionality}</h6>
+                                                    <h6 class="fs-6">Director: ${peli.director}</h6>
+                                                    <h6 class="fs-6">Guionista: ${peli.writer}</h6>
+                                                    <h6 class="fs-6">Genero: ${peli.genre}</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>`)
+        }
+
+        for(let prof of arrProf){
+            $(".cuerpoProf").append(`<div class="card mb-3 bg-secondary d-flex justify-content-center align-items-center"
+                                        style="max-width: 400px;min-height: 250px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4 ps-2 d-flex justify-content-center align-items-center foto">
+                                                <img src="${prof.photo}" class="img-fluid rounded-start" alt="img">
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h5 class="card-title text-black">${prof.name}</h5>
+                                                    <h6 class="fs-6">Edad: ${prof.age}</h6>
+                                                    <h6 class="fs-6">Nacionalidad: ${prof.nationality}</h6>
+                                                    <h6 class="fs-6">Osacars: ${prof.oscarsNumber}</h6>
+                                                    <h6 class="fs-6">Profesion: ${prof.profession}</h6>
+                                                    <h6 class="fs-6">Esta retirado: ${prof.retirado}</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>`)
         }
     })
+
 
     $("#boton").on("click", () => {
 
@@ -39,7 +70,7 @@ jQuery(() => {
         style="max-width: 400px;">
         <div class="row g-0">
             <div class="col-md-4 ps-2 d-flex justify-content-center align-items-center foto">
-                <img src="./imagenes/la_vida_de_brian.jfif" class="img-fluid rounded-start" alt="img">
+                <img src="${arrPelis[arrPelis.length -1].photo}" class="img-fluid rounded-start" alt="img">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
--- proyectoFinalFrontEnd/professional.js
+++ proyectoFinalFrontEnd/professional.js
@@ -1,14 +1,14 @@
-class Professional{
-    constructor(name, age, nationality, oscarsNumber, profession, photo){
+export class Professional{
+    constructor(name, age, nationality, oscarsNumber, profession, retirado, photo){
         this.name = name
         this.age = age
         this.nationality = nationality
         this.oscarsNumber = oscarsNumber
         this.profession = profession
+        this.retirado = retirado
         this.photo = photo
     }
     toString(){
         console.log(this);    
     }
 }
-module.exports = {Professional}
